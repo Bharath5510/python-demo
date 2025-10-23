2854f21244dca2c721028d71dd1ec141019e2888
Bharath5510 <119494373+Bharath5510@users.noreply.github.com>
2025-10-23 12:53:44 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    x = 10
+    y = 5
+    print("Addition:", add(x, y))
+    print("Subtraction:", sub(x, y))
