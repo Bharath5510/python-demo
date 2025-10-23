8a993ce2802fde7ef929b499be51fab869a03f1d
Bharath5510 <119494373+Bharath5510@users.noreply.github.com>
2025-10-23 14:41:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ def sub(a, b):
     return a - b
 
 if __name__ == "__main__":
-    x = 10
-    y = 5
-    print("Addition:", add(x, y))
-    print("Subtraction:", sub(x, y))
+    with open("output.txt", "w") as f:
+        f.write(f"Addition: {add(10, 5)}\n")
+        f.write(f"Subtraction: {sub(10, 5)}\n")
